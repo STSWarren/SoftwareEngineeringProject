323d4d2566fa3e38c2aa3345562de31cdf879a84
Stuart Samuel Warren <warrenst@lg12l39.scss.tcd.ie>
2017-10-20 16:15:27 +0100

    Change Tree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -3,11 +3,11 @@ public class BinaryTree{
 	private int counter = 0;
 	
 	public BinaryTree(int data){
-		this.root = new TreeNode(""+counter,true);
+		this.root = new TreeNode(""+data,true);
 		this.counter++;
 	}
 	
-	public void add(TreeNode parent, TreeNode child, String orientation){
+	public void add(TreeNode parent,TreeNode child, String orientation){
 		if (orientation.equals("left"))
         {
            parent.setLeftChild(child);
--- src/main.java
+++ src/main.java
@@ -1,12 +1,27 @@
 
 public class main {
 	public static void main(String args[]){
+		BinaryTree tree = new BinaryTree(3);
 		
 	}
 	
-	public TreeNode[] createTreeOfSizeN(int n){
-		TreeNode[] tree = new TreeNode[n];
+	public TreeNode findLCA(TreeNode r, int n, int l){
+		if(r == null){
+			return null;
+		}
+		if(r.getID().equals(""+n)||r.getID().equals(""+l)){
+			return r;
+		}
 		
-		return tree;
+		TreeNode left = findLCA(r.getLeftChild(),n,l);
+		TreeNode right = findLCA(r.getRightChild(),n,l);
+		if(left!=null&&right!=null){
+			return r;
+		}
+		if(left!=null){
+			return left;
+		}else{
+			return right;
+		}
 	}
 }
